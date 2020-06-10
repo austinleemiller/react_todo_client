5fa097b50b9c01cc75463c29b9348a645621da23
austinleemiller <mountainmcoding@gmail.com>
2020-06-10 11:33:02 -0600

    added axios, added ability to post todo to api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component{
- constructor(){
-   super()
- }
- render(){
-   return(
-   <div className="app">
-     <h1>ToDo List</h1>
-   </div>
-   )
- }
+import './styles.css'
+import TodoItem from './Todoitem'
+import axios from 'axios'
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todo: "",
+      todos: []
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      todo: e.target.value
+    })
+  }
+
+  addTodo = (e) => {
+    e.preventDefault()
+    axios({
+      method: "post",
+      url: "http://localhost:5000/todo",
+      headers: {"content-type": "application/json"},
+      data: {
+        title: this.state.todo,
+        done: false
+      }
+    })
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:5000/todos")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        this.setState({
+          todos: data
+        })
+      })
+      .catch(err => {
+        console.log("FetchTodos Error:", err)
+      })
+  }
+
+  renderTodos = () => {
+    return this.state.todos.map(todo => {
+      return <TodoItem key={todo.id} todoData={todo} />
+    })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>ToDo List</h1>
+        <form className="add-todo" onSubmit={this.addTodo}>
+          <input
+            type="text"
+            placeholder="Add Todo"
+            onChange={this.handleChange}
+            value={this.state.todo}
+          />
+          <button type="submit">Add</button>
+        </form>
+        {this.renderTodos()}
+      </div>
+    )
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
